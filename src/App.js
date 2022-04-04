9c66cef3cfa4c2c242edebae872aa9358b8ba5da
Annise Arif <83833939+annise-arif@users.noreply.github.com>
2022-04-04 19:52:05 +0600

    added home

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Header from './component/Header/Header';
+import Reviews from './component/Reviews/Reviews';
+import Home from './component/Home/Home';
+import Dashboard from './component/Dashboard/Dashboard';
+import Blogs from './component/Blogs/Blogs';
+import About from './component/About/About';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header></Header>
       <Routes>
-        <Route path='/' element={<Header></Header>}></Route>
-        <Route></Route>
-        <Route></Route>
-        <Route></Route>
-        <Route></Route>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/reviews' element={<Reviews></Reviews>}></Route>
+        <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
       </Routes>
     </div>
   );
---
+++ src/component/Home/Home.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import CustomerReviews from '../CustomerReviews/CustomerReviews';
+import './Home.css';
+
+const Home = () => {
+    return (
+        <div className='main-container'>
+            <div className='home-container'>
+                <div className="product-update">
+                    <h1>Your Next Headphone <br />
+                    <span>Your best Headphone</span>
+                    </h1>
+                    <p>
+                    The best headphones deliver the very best value for your
+                    money — but that doesn't mean these are all budget buys.
+                    While we have included some of our favorite headphone bargains,
+                    sometimes the best headphones listed here are based on an 
+                    outstanding feature set,
+                    </p>
+                    <button>Live demo</button>
+                </div>
+              <div className="img-container">
+                <img src="https://i.pinimg.com/originals/8c/bf/15/8cbf154777218d5663ea92514af35bb2.jpg" alt="" />
+              </div>
+           </div>
+
+           {/* customer review section */}
+
+           <div className="customer-reviews">
+              <CustomerReviews></CustomerReviews>
+           </div>
+
+        </div>
+    );
+};
+
+export default Home;
